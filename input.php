562accde153268063da1689c5a48ea6cd9c17483
tetsu-upstr <bodomu@gmail.com>
2020-02-29 08:31:28 +0900

    CSRF対策

--- input.php
+++ input.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 
 // スーパーグローバル変数 nameの値がkey valueの値がvalue
 // 連想配列
@@ -6,6 +7,14 @@
 
 //フォームの作り方 input.php confirm.php thanks.php ページを切り替えていく方法と
 // input.php 1つのファイルで作る方法
+
+// クリックジャッキング対策
+header('X-FRAME-OPTIONS:DENY');
+
+// サニタイズ関数 xxs対策
+function h($str) {
+  return htmlspecialchars($str, ENT_QUOTES, "UTF-8");
+}
 
 echo '<pre>';
 var_dump($_POST);
@@ -33,20 +42,24 @@ if (!empty($_POST['btn_submit'])) {
 </head>
 <body>
 
-<!-- フォーム入力画面 -->
+<!-- 確認画面 -->
 <?php if ($pageFlag === 1) : ?>
+  <!-- セッションのトークンとPOSTで渡ってきたトークンが同じなら確認画面を表示する -->
+  <?php if ($_POST['csrf'] === $_SESSION['csrfToken']) : ?>
+
   <form method="POST" action="input.php">
   名前
-  <?php echo $_POST['your_name']; ?>
+  <?php echo h($_POST['your_name']); ?>
   <br>
   メールアドレス
-  <?php echo $_POST['email']; ?>
+  <?php echo h($_POST['email']); ?>
 
   <input type="submit" name="back" value="戻る">
   <input type="submit" name="btn_submit" value="送信する">
-  <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
-  <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
+  <input type="hidden" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
+  <input type="hidden" name="email" value="<?php echo h($_POST['email']); ?>">
   </form>
+  <?php endif; ?>
 <?php endif; ?>
 
 <!-- 送信完了画面 -->
@@ -54,15 +67,25 @@ if (!empty($_POST['btn_submit'])) {
 送信が完了しました
 <?php endif; ?>
 
-<!-- 確認画面 -->
+<!-- フォーム入力画面 -->
 <?php if ($pageFlag === 0) : ?>
+<?php
+if (!isset($_SESSION['csrfToken'])) {
+  // CSRF対策のトークンを16進数で変換
+  $csrfToken = bin2hex(random_bytes(32));
+  $_SESSION['csrfToken'] = $csrfToken;
+}
+$token = $_SESSION['csrfToken'];
+?>
+
   <form method="POST" action="input.php">
   名前
-  <input type="text" name="your_name" value="<?php echo $_POST['your_name']; ?>">
+  <input type="text" name="your_name" value="<?php echo h($_POST['your_name']); ?>">
   <br>
   メールアドレス
-  <input type="email" name="email" value="<?php echo $_POST['email']; ?>">
+  <input type="email" name="email" value="<?php echo h($_POST['email']); ?>">
   <input type="submit" name="btn_confirm" value="確認する">
+  <input type="hidden" name="csrf" value="<?php echo $token; ?>">
   </form>
 <?php endif; ?>
 
