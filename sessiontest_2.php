3553a5751e6334c2b93e07bd569d3e0e1f35398b
tetsu-upstr <bodomu@gmail.com>
2020-03-01 15:04:10 +0900

    セッションとクッキー

---
+++ sessiontest_2.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+
+</head>
+<body>
+  
+<?php
+
+echo 'セッション破棄しました。';
+
+// 空で上書きする
+$_SESSION = [];
+
+if (isset($_COOKIE['PHPSESSID'])) {
+  setcookie('PHPSESSID', '', time() - 1800, '/');
+}
+
+session_destroy();
+
+echo 'セッション';
+echo '<pre>';
+var_dump($_SESSION);
+echo '</pre>';
+
+echo 'クッキー';
+echo '<pre>';
+var_dump($_COOKIE);
+echo '</pre>';
+
+
+
+?>
+</body>
+</html>
