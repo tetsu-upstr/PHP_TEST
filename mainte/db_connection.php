aa1a6c7d873ebfa52739a41fbdf2c55e6ba6049f
tetsu-upstr <bodomu@gmail.com>
2020-03-01 10:51:51 +0900

    DB接続

---
+++ mainte/db_connection.php
@@ -0,0 +1,20 @@
+<?php
+
+const DB_HOST = 'mysql:dbname=udemy_php;host=127.0.0.1';
+const DB_USER = 'php_user';
+const DB_PASSWORD = 'password123';
+
+// 例外 exceotion
+try {
+  $pdo = new PDO(DB_HOST, DB_USER, DB_PASSWORD, [
+    // PDOオプション []
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FECTH_ASSOC, // 連想配列
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // 例外
+    PDO::ATTR_EMULATE_PREPARES => false, // SQLインジェクション
+  ]);
+  echo '接続成功';
+
+} catch(PDOException $e) {
+  echo '接続失敗' . $e->getMessage() . "\n";
+  exit();
+}
